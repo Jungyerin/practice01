7372edd58a12f11141a8fd7faa23bcab4f42c166
JYL <JYL@DESKTOP-TORHHDE>
2017-04-15 20:24:46 +0900

    170415

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -10,6 +10,23 @@ public class Prob5 {
 
 			String s = String.valueOf(i);
 
+			for (int j = 0; j < s.length(); j++) {
+				if (s.charAt(j) == '3' || s.charAt(j) == '6' || s.charAt(j) == '9') {
+					count++;
+				}
+			}
+
+			if (count == 0) {
+				continue;
+			} else {
+				System.out.print(s + " ");
+				for (int z = 0; z < count; z++) {
+					System.out.print("짝");
+				}
+			}
+			count = 0;
+			System.out.println();
+
 		}
 	}
 
